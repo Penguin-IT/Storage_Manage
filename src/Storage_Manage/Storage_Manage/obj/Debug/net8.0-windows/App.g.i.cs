7398c5e73ca31cbe5566eec3fc39bdcdd54104a4
ngiathinh346 <ngiathinh346@gmail.com>
2026-05-05 18:02:38 +0700

    UI Form tim kiem và thong ke

--- src/Storage_Manage/Storage_Manage/obj/Debug/net8.0-windows/App.g.i.cs
+++ src/Storage_Manage/Storage_Manage/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Storage_Manage {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.12.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Storage_Manage {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.12.0")]
         public static void Main() {
             Storage_Manage.App app = new Storage_Manage.App();
             app.InitializeComponent();
